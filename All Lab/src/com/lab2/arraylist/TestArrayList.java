8cedd02bc407e23b0e51c135e0ccdabe997bace2
Satthum34 <Satthum34@Satthum>
2014-04-19 10:51:40 +0700

    sdfsfd

---
+++ All Lab/src/com/lab2/arraylist/TestArrayList.java
@@ -0,0 +1,17 @@
+package com.lab2.arraylist;
+
+public class TestArrayList {
+
+	public static void main(String[] args) {
+		ArrayLisst arr = new ArrayLisst();
+		arr.add("1");
+		System.out.println(arr);
+		arr.add("2");
+		System.out.println(arr);
+		arr.add("3");
+		System.out.println(arr);
+		arr.add(2, "4");
+		System.out.println(arr);
+	}
+
+}
